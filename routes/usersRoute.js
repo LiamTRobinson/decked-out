433024d9f12b182847549698ccdd2ca88f9ad660
LiamTRobinson <Liamtrobinson@gmail.com>
2017-03-24 17:27:48 -0400

    create new user post route

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -12,9 +12,20 @@ router.get("/createaccount", function(req, res){
 	res.render("users/new");
 });
 
-//user post route
+//user new post route
 router.get("/", function(req, res) {
-
+	var user = new User ({
+		email: req.body.email,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		userName: req.body.userName,
+		password: req.body.password
+	});
+	user.save(function(err, user) {
+		if (err) { console.log(err); }
+		console.log(user);
+		res.redirect("/sessions/login");
+	});
 });
 
 module.exports = router;
