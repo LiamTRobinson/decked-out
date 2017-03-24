5596953967758c69f4a7864aff828f9ebd363afa
LiamTRobinson <Liamtrobinson@gmail.com>
2017-03-24 15:15:14 -0400

    clean up app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//packages
 var express = require('express');
 require('dotenv').config()
 var path = require('path');
@@ -8,9 +9,7 @@ var bodyParser = require('body-parser');
 var hbs = require("hbs");
 var methodOverride = require("method-override");
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+//express set
 var app = express();
 
 //mongoose stuff
@@ -24,9 +23,18 @@ db.on('error', function(err){
 db.once('open', function() {
   console.log("database has been connected!");
 });
+
+//require routes
+var index = require('./routes/index');
+var users = require('./routes/users');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+//use routes
+app.use('/', index);
+app.use('/users', users);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -36,9 +44,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
