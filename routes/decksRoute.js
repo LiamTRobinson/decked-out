40cf7fe442886477cc00f0fb4f3a14cb7dea2bac
LiamTRobinson <Liamtrobinson@gmail.com>
2017-03-26 11:00:11 -0400

    implement materialize framework

--- routes/decksRoute.js
+++ routes/decksRoute.js
@@ -88,27 +88,48 @@ router.patch("/:id/edit", function(req, res) {
 
 //DECKS ADD CARD GET ROUTE
 router.get("/:id/edit/addcard", function(req, res) {
-	User.getById(req.params.userId)
+	User.findById(req.params.userId)
 		.exec(function(err, user) {
-			var cards = user.cards;
+			var cards = user.cards;			
 			var deck = user.decks.id(req.params.id);
-			var cardsToShow = deck.filter(function(card) {
-				for (var i = 0; i < cards.length; i++) {
-					if (card.id === cards[i].id) {
-						return false
+			var filterFunction = function(card) {
+				if (deck.mainDeck.length !== 0) {
+					for (var i = 0; i < deck.mainDeck.length; i++) {
+						if (card.id === deck.mainDeck[i].id) {
+							return false;
+						}
 					}
 				}
 				return true;
-			});
+			}
+			var cardsToShow = cards.filter(filterFunction);
 			res.render("decks/addcard", {
 				cards: cardsToShow,
 				user: req.params.userId,
 				deck: req.params.id
 			});
 		});
-	
 });
 
-//DECKS ADD CARD POST ROUTE
+//DECKS ADD CARD PATCH ROUTE
+router.patch("/:id/edit/addcard/:cardId", function(req, res) {
+	var cardToAdd = null;
+	User.findById(req.params.userId)
+		.exec(function(err, user) {
+			var deckToEdit = user.decks.id(req.params.id);
+			console.log(deckToEdit);
+			cardToAdd = user.cards.id(req.params.cardId);
+			console.log(cardToAdd);
+			deckToEdit.mainDeck.push(cardToAdd);
+			user.save();
+		Deck.findById(req.params.id)
+			.exec(function(err, deck) {
+				deck.mainDeck.push(cardToAdd);
+				deck.save();
+				res.redirect(`/${req.params.userId}/decks/${req.params.id}/edit`);
+			});	
+		});
+	
+});
 
 module.exports = router;
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 //HOME PAGE
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {
+  	menuOne: "Log In",
+  	menuTwo: "Create Account",
+  	menuOnehref: "/sessions/login",
+  	menuTwohref: "/users/createaccount"
+  });
 });
 
 module.exports = router;
