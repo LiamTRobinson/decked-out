c99ded1f69cb2e29d374dc8280480208245e2fac
LiamTRobinson <Liamtrobinson@gmail.com>
2017-03-27 16:57:35 -0400

    complete sample hand feature, add total cards to deck show view

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){   
     $('.modal').modal();
     $('.carousel').carousel();
+    $("#sample-hand-trigger").on("click", function() {
+    	var data = $(this).data("store");
+    	var array = data.split(",");
+    	for (var i = 0; i < 7; i++) {
+    		var cardIndex = Math.floor(Math.random() * array.length);
+    		var card = array[cardIndex];
+    		$(`#sh-card-${i}`).attr("src", card);
+    		array.splice(cardIndex, 1);
+    	}
+    });
   });
